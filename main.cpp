dfaf937c0bab13353e5019d8efa251bb3bd339b2
= <=>
2025-06-17 09:18:35 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string encrypt(string text, int key)
+{
+    string result = "";
+
+    for(char c : text){
+        if(isalpha(c)){
+            char base = 'a';
+            c = (c - base + key) % 26 + base;
+            result += c; 
+        }else{
+           result += c;  
+        }
+    }
+
+    return result;
+}
+
+string decrypt(string text, int shift) {
+    return encrypt(text, 26 - shift);  // Reverse the shift
+}
+
+
+int main()
+{
+    string text;
+    cout<<"Enter Text to be Encrypted / Decrypted : ";
+    getline(cin, text);
+
+    int key;
+    cout<<"Enter Key : ";
+    cin>>key;
+
+    string ciphertext = decrypt(text, key);
+
+    cout<<"The cipher text is : "<<ciphertext<<endl;
+    return 0;
+}
