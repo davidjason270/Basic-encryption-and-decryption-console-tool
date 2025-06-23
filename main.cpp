9166492356074d5e8340011848139b2a0768e702
= <=>
2025-06-23 19:40:25 +0200

    menu added to the app

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
 
 using namespace std;
 
-string encrypt(string text, int key)
+
+string caeser_cipher(string text, int key)
 {
     string result = "";
 
@@ -20,8 +22,8 @@ string encrypt(string text, int key)
     return result;
 }
 
-string decrypt(string text, int shift) {
-    return encrypt(text, 26 - shift);  // Reverse the shift
+string decrypt(string text, int key) {
+    return caeser_cipher(text, 26 - key);  // Reverse the key
 }
 
 string atbash(string text)
@@ -43,12 +45,55 @@ string atbash(string text)
 
 int main()
 {
-    string text;
-    cout<<"Enter Text to be Encrypted / Decrypted : ";
-    getline(cin, text);
+    while(true){
+        cout << "\n---------------------------------\n";
+        cout << "Basic Encryption / Decrytion Tool\n";
+        cout << "---------------------------------\n";
+        cout << "Encrypt or Decrypt your message using\n";
+        cout << "a. Caeser Cipher\n";
+        cout << "b. Atbash Cipher\n";
+        cout << "c. Reverse Cipher\n";
+        cout << "d. Rot13 Cipher\n";
+        cout << "e. Exit\n";
 
-    string ciphertext = atbash(text);
+        char choice;
+        cout<<"Enter an Option from a - d:";
+        cin>>choice;
 
-    cout<<"The cipher text is : "<<ciphertext<<endl;
+        if(choice == 'a' || choice == 'A'){
+             system("cls");
+            cout<<"\n\nCaeser Cipher\n\n";
+
+            cout<<"Enter the key:";
+            int key; cin>>key;
+            cin.clear(); cin.ignore();
+
+            cout<<"Enter a message:";
+            string message; 
+            getline(cin, message);
+
+            string cipher = caeser_cipher(message, key);
+
+            cout<<"The Cipher text is: "<< cipher<<endl;
+            system("pause");
+
+
+        }else if(choice == 'b' || choice == 'B'){
+             system("cls");
+            cout<<"\n\nAtBash Cipher\n\n";
+        }else if(choice == 'c' || choice == 'C'){
+             system("cls");
+            cout<<"\n\nReverse Cipher\n\n";
+        }else if(choice == 'd' || choice == 'D'){
+             system("cls");
+            cout<<"\n\nROT13 Cipher\n\n";
+        }else if(choice == 'e' || choice == 'E'){
+             system("cls");
+            cout<<"\n\nGOOD BYE!\n\n";
+            exit(0);
+        }else{
+            cout<<"\n\nInvalid input\n\n";
+        }
+    }
     return 0;
 }
